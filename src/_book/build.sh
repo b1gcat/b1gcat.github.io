7fedcbf95d3c0dffb4854e7837aa74a072e8d297
b1gcat <b1gcat@b1gcat.local>
2022-04-20 09:09:04 +0800

    update

--- src/_book/build.sh
+++
@@ -1,9 +0,0 @@
-#npm install -g gitbook-cli
-#npm install -g gitbook-summary
-#npm install -g gitbook-plugin-simple-mind-map
-gitbook install
-book sm -i node_modules
-gitbook build
-cp -rf _book/* ../../b1gcat.github.io/
-rm -rf _book/*
-rm -f ../build.sh 
--- src/build.sh
+++ src/build.sh
@@ -1,9 +1,13 @@
 #npm install -g gitbook-cli
 #npm install -g gitbook-summary
 #npm install -g gitbook-plugin-simple-mind-map
+
 gitbook install
 book sm -i node_modules
 gitbook build
+
+rm -rf _book/build.sh
+rm -rf _book/.gitignore
 cp -rf _book/* ../../b1gcat.github.io/
-rm -rf _book/*
-rm -f ../build.sh 
+rm -rf _book
+
