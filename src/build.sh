7c09f952dce82fd2823c2690e439911c6b76e2be
b1gcat <b1gcat@b1gcat.local>
2022-04-20 08:42:12 +0800

    update

--- src/build.sh
+++ src/build.sh
@@ -1,10 +1,9 @@
 #npm install -g gitbook-cli
 #npm install -g gitbook-summary
 #npm install -g gitbook-plugin-simple-mind-map
-#gitbook install
+gitbook install
 book sm -i node_modules
 gitbook build
 cp -rf _book/* ../../b1gcat.github.io/
 rm -rf _book/*
-rm -f ../attack.xmind
 rm -f ../build.sh 
